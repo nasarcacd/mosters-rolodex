913825f2ec2252c432780d9ba53216e6f2528592
Nayib Sarmiento <nayibsc@gmail.com>
2020-03-28 13:38:04 -0600

    Updates

---
+++ precache-manifest.0be5cfbef29d73da065bab60a17cb3f0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d782f43a2c18892cd956116c306c27b5",
+    "url": "/mosters-rolodex/index.html"
+  },
+  {
+    "revision": "601de4c5f561e41877cd",
+    "url": "/mosters-rolodex/static/css/main.1ac95ef5.chunk.css"
+  },
+  {
+    "revision": "1d6d326f4470ed67fe85",
+    "url": "/mosters-rolodex/static/js/2.46e2921d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/mosters-rolodex/static/js/2.46e2921d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "601de4c5f561e41877cd",
+    "url": "/mosters-rolodex/static/js/main.16c25791.chunk.js"
+  },
+  {
+    "revision": "a5f55355300c34ad5d10",
+    "url": "/mosters-rolodex/static/js/runtime-main.b28c1ddd.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/mosters-rolodex/static/media/logo.5d5d9eef.svg"
+  }
+]);
